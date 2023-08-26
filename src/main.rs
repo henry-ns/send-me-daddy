08c985f39d2531c4a1127acb6f3a8ef4014817fa
Henrique Miranda <enrque.ns@gmail.com>
2023-08-26 11:45:20 -0300

    chore: setup actix

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Hello<'a> {
+    message: &'a str
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json(Hello {
+        message: "Hello word"
+    })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
